5692e5e5a060d29190339e9322873d6f4839bc59
tinytransfem <n3rilp@gmail.com>
2024-02-26 16:00:53 +0100

    fix tooltip not showing for tag descriptions

--- src/main/java/org/infernalstudios/everydesc/gui/TooltipEvent.java
+++ src/main/java/org/infernalstudios/everydesc/gui/TooltipEvent.java
@@ -2,7 +2,7 @@ package org.infernalstudios.everydesc.gui;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.resources.language.I18n;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
@@ -20,7 +20,7 @@ public class TooltipEvent {
     public static void onItemTooltip(ItemTooltipEvent event) {
         String langKey = EverythingDescriptions.MOD_ID + "." + ForgeRegistries.ITEMS.getKey(event.getItemStack().getItem());
         if (I18n.exists(langKey)) {
-            event.getToolTip().add(Component.translatable(EverythingDescriptions.MOD_ID + ".tooltip", KeyMappings.DESCRIPTION_KEY.getTranslatedKeyMessage()).withStyle(ChatFormatting.GRAY));
+            event.getToolTip().add(new TranslatableComponent(EverythingDescriptions.MOD_ID + ".tooltip", KeyMappings.DESCRIPTION_KEY.getTranslatedKeyMessage()).withStyle(ChatFormatting.GRAY));
         }
         else {
             List<TagKey<Item>> tags = event.getItemStack().getTags().toList();
@@ -33,7 +33,7 @@ public class TooltipEvent {
                 dummyArr1 = t.toString().split(" / ");
                 dummyArr2 = dummyArr1[1].split("]");
                 if (I18n.exists(EverythingDescriptions.MOD_ID + "." + dummyArr2[0] + ".tag")) {
-                    event.getToolTip().add(Component.translatable(EverythingDescriptions.MOD_ID + ".tooltip", KeyMappings.DESCRIPTION_KEY.getTranslatedKeyMessage()).withStyle(ChatFormatting.GRAY));
+                    event.getToolTip().add(new TranslatableComponent(EverythingDescriptions.MOD_ID + ".tooltip", KeyMappings.DESCRIPTION_KEY.getTranslatedKeyMessage()).withStyle(ChatFormatting.GRAY));
                     break;
                 }
             }
